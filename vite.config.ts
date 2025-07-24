98e79904821ac5633f84c02237c631929119bcde
MihirGujarati <mgujara2@asu.edu>
2025-07-24 03:28:23 +0000

    url change

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/web-dev-skill/',  // Set the base to match your GitHub repo name
+  base: '/Portfolio/',  // Set the base to match your GitHub repo name
 })
